f7dd4df0c198ba97239c10fd92f022b1516badcd
Janjusevic <91884372+Janjusevic@users.noreply.github.com>
2021-12-20 21:24:02 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -19,6 +19,6 @@ Config.UpaliBlip = true
 
 -- LOGOVI
 Config.Ikonica = "" -- Ovde Mozete po zelji da stavite ako zelite da bot koji salje log ima neku iknocu npr vas logo
-Config.WebHuk = "https://discord.com/api/webhooks/911651901018374206/O-7PkphKdbiJarPclymMq8o9PmMpnLp2jGoXHLBVM8hmYwfA2J3c1JlgWGoE5H5sU1QY" -- Ovde stavljate vas diskord webhook radi logova
+Config.WebHuk = "" -- Ovde stavljate vas diskord webhook radi logova
 Config.ImeBota = "Pranje Para Logovi" -- Ovde stavljate ime bota koji ce vam slati log
 
