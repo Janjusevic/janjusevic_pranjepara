a7265cc1269cf058eb4b8fa359c39134bb827da7
Janjusevic011 <91884372+Janjusevic@users.noreply.github.com>
2022-02-27 22:52:57 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,30 +1,20 @@
 fx_version 'adamant'
 games { 'gta5' }
 
-description 'Developer: Janjusevic Discord: Janjusevic#0001'
-version '0.1.2'
+description 'Sistem pranja para napravljen za ESX Framework'
+author 'Janjusevic'
 
 client_scripts {
-
 	'@es_extended/locale.lua',
 	'prevod/rs.lua',
 	'config.lua',
 	'pranje-cl.lua',
-
 }
 
 server_scripts {
-
 	'@es_extended/locale.lua',
 	'prevod/rs.lua',
 	'config.lua',
 	'pranje-sv.lua',
 	'version.lua'
-
-}
-
-dependencies {
-
-    'es_extended',
-
 }
